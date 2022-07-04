6c3afc0b5c26e659b50cc89b9e4717780b1a48ee
Nozhan Amiri <nozhana@gmail.com>
2022-07-04 13:39:03 +0430

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Nozhan Amiri on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
